3b629227472b221b8ac7ef4b8f86700bff80e5bf
Jon McDuffie <jon@jrmcduffie.com>
2025-01-16 20:18:01 -0500

    PHYSICSgit add --allgit add --all

--- ch_01/src/bouncing_ball.rs
+++ ch_01/src/bouncing_ball.rs
@@ -20,45 +20,89 @@ struct Model {
     query_pipeline: QueryPipeline,
     physics_hooks: (),
     event_handler: (),
-    ball_body_handle: RigidBodyHandle,
 }
 
-const BALL_RADIUS: f32 = 20.0;
+const BALL_RADIUS: f32 = 2.0;
 const BALL_COEFFICIENT_OF_RESTITUTION: f32 = 1.0;
-const LEFT_FLOOR_COEFFICIENT_OF_RESTITUTION: f32 = 0.9;
-const RIGHT_FLOOR_COEFFICIENT_OF_RESTITUTION: f32 = 0.5;
-const INITIAL_ALTITUDE: f32 = 300.0;
-const LEFT_WALL_ROTATION: f32 = -PI / 4.0;
-const LEFT_WALL_LENGTH: f32 = 600.0;
-const RIGHT_WALL_ROTATION: f32 = PI / 4.0;
-const RIGHT_WALL_LENGTH: f32 = 600.0;
+const LEFT_BLADE_COEFFICIENT_OF_RESTITUTION: f32 = 1.0;
+const RIGHT_BLADE_COEFFICIENT_OF_RESTITUTION: f32 = 1.0;
+const INITIAL_ALTITUDE: f32 = 30.0;
+const LEFT_BLADE_ROTATION: f32 = -PI / 4.0;
+const LEFT_BLADE_LENGTH: f32 = 40.0;
+const LEFT_BLADE_THICKNESS: f32 = 1.0;
+const RIGHT_BLADE_ROTATION: f32 = PI / 4.0;
+const RIGHT_BLADE_LENGTH: f32 = 40.0;
+const RIGHT_BLADE_THICKNESS: f32 = 1.0;
+const SCALE_FACTOR: f32 = 10.0;
 
 fn model(app: &App) -> Model {
     let mut rigid_body_set = RigidBodySet::new();
     let mut collider_set = ColliderSet::new();
 
-    /* Create the ground. */
-    let left_wall = ColliderBuilder::cuboid(LEFT_WALL_LENGTH / 2.0, 1.0)
-        .rotation(LEFT_WALL_ROTATION)
-        .restitution(LEFT_FLOOR_COEFFICIENT_OF_RESTITUTION)
+    let left_wall = ColliderBuilder::cuboid(1.0, 39.0)
+        .translation(vector![-40.0, 0.0])
+        .restitution(LEFT_BLADE_COEFFICIENT_OF_RESTITUTION)
+        .friction(0.0)
         .build();
     collider_set.insert(left_wall);
 
-    let right_wall = ColliderBuilder::cuboid(RIGHT_WALL_LENGTH / 2.0, 1.0)
-        .rotation(RIGHT_WALL_ROTATION)
-        .restitution(RIGHT_FLOOR_COEFFICIENT_OF_RESTITUTION)
+    let right_wall = ColliderBuilder::cuboid(1.0, 39.0)
+        .translation(vector![40.0, 0.0])
+        .restitution(LEFT_BLADE_COEFFICIENT_OF_RESTITUTION)
+        .friction(0.0)
         .build();
     collider_set.insert(right_wall);
 
+    let ceiling = ColliderBuilder::cuboid(39.0, 1.0)
+        .translation(vector![0.0, 40.0])
+        .restitution(LEFT_BLADE_COEFFICIENT_OF_RESTITUTION)
+        .friction(0.0)
+        .build();
+    collider_set.insert(ceiling);
+
+    let floor = ColliderBuilder::cuboid(39.0, 1.0)
+        .translation(vector![0.0, -40.0])
+        .restitution(LEFT_BLADE_COEFFICIENT_OF_RESTITUTION)
+        .friction(0.0)
+        .build();
+    collider_set.insert(floor);
+
+    let fan = RigidBodyBuilder::dynamic()
+        .lock_translations()
+        .angvel(PI / 2.0)
+        .angular_damping(0.0)
+        .linear_damping(0.0)
+        .build();
+    let left_blade_collider =
+        ColliderBuilder::cuboid(LEFT_BLADE_LENGTH / 2.0, LEFT_BLADE_THICKNESS / 2.0)
+            .rotation(LEFT_BLADE_ROTATION)
+            .restitution(LEFT_BLADE_COEFFICIENT_OF_RESTITUTION)
+            .friction(0.0)
+            .build();
+
+    let right_blade_collider =
+        ColliderBuilder::cuboid(RIGHT_BLADE_LENGTH / 2.0, RIGHT_BLADE_THICKNESS / 2.0)
+            .rotation(RIGHT_BLADE_ROTATION)
+            .restitution(RIGHT_BLADE_COEFFICIENT_OF_RESTITUTION)
+            .friction(0.0)
+            .build();
+
+    let fan_handle = rigid_body_set.insert(fan);
+    collider_set.insert_with_parent(left_blade_collider, fan_handle, &mut rigid_body_set);
+    collider_set.insert_with_parent(right_blade_collider, fan_handle, &mut rigid_body_set);
+
     /* Create the bouncing ball. */
-    let rigid_body = RigidBodyBuilder::dynamic()
-        .translation(vector![0.0, INITIAL_ALTITUDE])
-        .build();
-    let collider = ColliderBuilder::ball(BALL_RADIUS)
-        .restitution(BALL_COEFFICIENT_OF_RESTITUTION)
-        .build();
-    let ball_body_handle = rigid_body_set.insert(rigid_body);
-    collider_set.insert_with_parent(collider, ball_body_handle, &mut rigid_body_set);
+    for _ in 0..10 {
+        let rigid_body = RigidBodyBuilder::dynamic()
+            .translation(vector![random_range(-1.0, 1.0), INITIAL_ALTITUDE])
+            .build();
+        let collider = ColliderBuilder::ball(BALL_RADIUS)
+            .restitution(BALL_COEFFICIENT_OF_RESTITUTION)
+            .friction(0.0)
+            .build();
+        let ball_body_handle = rigid_body_set.insert(rigid_body);
+        collider_set.insert_with_parent(collider, ball_body_handle, &mut rigid_body_set);
+    }
 
     /* Create other structures necessary for the simulation. */
     let gravity = vector![0.0, -9.81];
@@ -76,7 +120,6 @@ fn model(app: &App) -> Model {
 
     let _window = app.new_window().size(800, 800).view(view).build().unwrap();
     Model {
-        ball_body_handle,
         gravity,
         integration_parameters,
         rigid_body_set,
@@ -110,9 +153,6 @@ fn update(_app: &App, model: &mut Model, _update: Update) {
         &model.physics_hooks,
         &model.event_handler,
     );
-
-    let ball_body = &model.rigid_body_set[model.ball_body_handle];
-    println!("Ball altitude: {}", ball_body.translation().y);
 }
 
 fn view(app: &App, model: &Model, frame: Frame) {
@@ -120,22 +160,33 @@ fn view(app: &App, model: &Model, frame: Frame) {
 
     draw.background().color(WHITE);
 
-    let position = model.rigid_body_set[model.ball_body_handle].translation();
-
-    draw.ellipse()
-        .x_y(position.x, position.y)
-        .radius(BALL_RADIUS)
-        .rgba(0.5, 0.5, 0.5, 1.0)
-        .stroke(BLACK);
-
-    draw.rect()
-        .w_h(LEFT_WALL_LENGTH, 2.0)
-        .rotate(LEFT_WALL_ROTATION)
-        .color(BLACK);
-    draw.rect()
-        .w_h(RIGHT_WALL_LENGTH, 2.0)
-        .rotate(RIGHT_WALL_ROTATION)
-        .color(BLACK);
+    for (_, collider) in model.collider_set.iter() {
+        let position = collider.position().translation.vector;
+        let shape = collider.shape();
+        // change color if there was a collision
+        match shape.shape_type() {
+            ShapeType::Ball => {
+                let ball = shape.as_ball().unwrap();
+                draw.ellipse()
+                    .x_y(position.x * SCALE_FACTOR, position.y * SCALE_FACTOR)
+                    .radius(ball.radius * SCALE_FACTOR)
+                    .rgba(0.5, 0.5, 0.5, 1.0)
+                    .stroke(BLACK);
+            }
+            ShapeType::Cuboid => {
+                let cuboid = shape.as_cuboid().unwrap();
+                draw.rect()
+                    .x_y(position.x * SCALE_FACTOR, position.y * SCALE_FACTOR)
+                    .w_h(
+                        cuboid.half_extents.x * 2.0 * SCALE_FACTOR,
+                        cuboid.half_extents.y * 2.0 * SCALE_FACTOR,
+                    )
+                    .rotate(collider.position().rotation.angle())
+                    .color(BLACK);
+            }
+            _ => {}
+        }
+    }
 
     draw.to_frame(app, &frame).unwrap();
 }
